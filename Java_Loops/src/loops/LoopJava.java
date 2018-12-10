79872913f1c0c635cd30c6aec0e283da2eeb22e0
Aasik <aasikmohamed94@gmail.com>
2018-12-11 00:21:28 +0530

    Add files via upload

---
+++ Java_Loops/src/loops/LoopJava.java
@@ -0,0 +1,22 @@
+package loops;
+
+public class LoopJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Create an array contains same type of elements 
+		String arr[] = {"You","are","awesome","on","Your","own"};
+		
+		int i ;
+		
+		//iterating over an array
+		for( i = 0; i < arr.length; i++ ) {
+			
+			//accessing each element of an array
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
